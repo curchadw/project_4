02a9494da8481a0eb9e9e7894e76e958e56fc036
curtis <laurencechadwell@gmail.com>
2020-10-19 19:36:16 -0500

    js file is not linking

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,106 +1,121 @@
-import { Owner } from './owner.js';
-import { Property } from './property.js'
+// import { Owner } from './owner.js';
+// import { Property } from './property.js'
+
+
 
 const BASE_URL = "http://localhost:3000"
 const OWNERS_URL = `${BASE_URL}/owners`
 const PROPERTIES_URL = `${BASE_URL}/properties`
-const BACKEND_URL = 'localhost:3000';
-const submitBtn = document.querySelector('#submit')
+
 
 document.addEventListener('DOMContentLoaded',() => {
     
-    
+    let btn = document.getElementById('btn')
+    btn.addEventListener('click',()=>{
+        alert('hi');
+    })
     
    });
 
 
-//    submitBtn.addEventListener('submit',{
-//        if(event.target.id === 'submit'){
-           
-//        }
-//    })
-
-   const addListing = () => {
-
-        return fetch(PROPERTIES_URL,{
-            method: 'POST',
-            headers: {
-                "Content-Type": "application/json",
-                "Accept": "application/json" 
-            },
-            body:JSON.stringify({'owner_id': ownerId})
-        })
-        .then((resp) => resp.json())
-        .then((json) =>{console.log(json)})
-        .catch((error) =>console.log(error))
-
-   }
 
 
-   const addOwner = () => {
+//    function addListing() {
 
-    return fetch(OWNERS_URL,{
-        method: 'POST',
-        headers: {
-            "Content-Type": "application/json",
-            "Accept": "application/json" 
-        },
-        body:JSON.stringify({'owner_id': ownerId})
-    })
-    .then((resp) => resp.json())
-    .then((json) =>{console.log(json)})
-    .catch((error) =>console.log(error))
+//     return fetch(PROPERTIES_URL, {
+//         method: 'POST',
+//         headers: {
+//             "Content-Type": "application/json",
+//             "Accept": "application/json"
+//         },
+//         body: JSON.stringify({ 'owner_id': ownerId })
+//     })
+//         .then((resp) => resp.json())
+//         .then((json) => { console.log(json); })
+//         .catch((error) => console.log(error));
 
-}
+// }
+
+
+//    const addOwner = () => {
+
+//     return fetch(OWNERS_URL,{
+//         method: 'POST',
+//         headers: {
+//             "Content-Type": "application/json",
+//             "Accept": "application/json" 
+//         },
+//         body:JSON.stringify({'owner_id': ownerId})
+//     })
+//     .then((resp) => resp.json())
+//     .then((json) =>{console.log(json)})
+//     .catch((error) =>console.log(error))
+
+// }
 
 
 
+let dropdown = document.getElementById('sel');
+dropdown.length = 0;
 
+let defaultOption = document.createElement('option');
+defaultOption.text = 'Choose owner';
 
+dropdown.add(defaultOption);
+dropdown.selectedIndex = 0;
 
+const url = OWNERS_URL;
 
+fetch(url)
+    .then((resp) =>{
+        if(resp.status !== 200){
+            console.warn('Looks like there was a problem. Status Code: ' + 
+          resp.status);  
+        return;    
+        }
+        resp.json().then((data) =>{  
+            let option;
+        
+            for (let i = 0; i < data.length; i++) {
+              option = document.createElement('option');
+                option.text = data[i].name;
+                dropdown.add(option);
+            }    
+          }); 
 
-
-
-
-async function getOwnerData(){
-    const ownerPromise = await fetch(OWNERS_URL)
-    const owner = await ownerPromise.json()
-    owner.forEach( owner => {
-        const name = this.name
-        const phone_number = this.phone_number
-        const real_estate_agent = this.real_estate_agent
-        main.innerHTML += `<div>
-                    <h2>Owner Name: ${name}</h2>
-                    <p>Phone Number: ${phone_number}</p>
-                    <p>Real Estate Agent: ${real_estate_agent}</p>
-                    <p>Current Properties: ${owner.properties.state}</p>
-        </div>
-        `
-    })
-
-    
-
-    function makeListingForm(){
-        return (`
-            <form>
-                <label>Owner Name:</label>
-                <input type="text" id="owner_name"><br>
-                <label>Phone Number:</label>
-                <input type="text" id="phone_number"><br>
-                <label>Real Estate Agent:</label>
-                <input type="text" id="agent"><br>
-                <input type ="submit">
-                
-            </form>
-            `)
     }
+    )
+    .catch(function(err) {  
+        console.error('Fetch Error -', err);  
+      });
+
+
+
+// async function getOwnerData(){
+//     const ownerPromise = await fetch(OWNERS_URL)
+//     const owner = await ownerPromise.json()
+//     owner.forEach( owner => {
+//         const name = this.name
+//         const phone_number = this.phone_number
+//         const real_estate_agent = this.real_estate_agent
+//         main.innerHTML += `<div>
+//                     <h2>Owner Name: ${name}</h2>
+//                     <p>Phone Number: ${phone_number}</p>
+//                     <p>Real Estate Agent: ${real_estate_agent}</p>
+//                     <p>Current Properties: ${owner.properties.state}</p>
+//         </div>
+//         `
+//     })
+
     
-}
+
+
+    
+// }
 
 
 
- 
+
 
 
 
